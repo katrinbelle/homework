885d6251117e4a0b91bc45e966fa8d4ad4eac58b
Ekaterina <kat.bommon@gmail.com>
2023-01-17 05:19:09 +0300

    task3 completion

---
+++ homework3/Program.cs
@@ -0,0 +1,11 @@
+﻿/*Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+4 -> да
+-3 -> нет
+7 -> нет*/
+
+Console.Write("Введите число: ");
+string numberstr = Console.ReadLine();
+int number = Convert.ToInt32(numberstr);
+ if (number%2 ==0) Console.WriteLine("Число "+" "+ number +" "+ "делится на 2 без остатка");
+ else Console.WriteLine("Число "+" "+ number +" "+ "не делится на 2 без остатка");
